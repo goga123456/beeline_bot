9601d68c55eed6a0dde90f691a59d78615550fd7
goga123456 <110159417+goga123456@users.noreply.github.com>
2023-02-20 14:20:54 +0500

    Update views.py

--- bot/views.py
+++ bot/views.py
@@ -349,7 +349,7 @@ def ask_language(message):
         user_dict[chat_id] = user
         between_language_and_about_resume(message)
     except KeyError:
-        bot.reply_to(message.chat.id,"Выберите один из вариантов 'Русский' или 'Ozbek tili'\n\n 'Русский' yoki 'Ozbek tili' parametrlaridan birini tanlang ")
+        bot.reply_to(message,"Выберите один из вариантов 'Русский' или 'Ozbek tili'\n\n 'Русский' yoki 'Ozbek tili' parametrlaridan birini tanlang ")
        
 
 
